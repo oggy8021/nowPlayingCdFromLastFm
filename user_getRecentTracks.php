3c9e43cf8c62c6a30363ba64c22bb2ad089987f6
oggy8021 <oggyist@gmail.com>
2011-08-19 08:31:59 +0000

    exampleコードからのテストコード.
    git-svn-id: file:///home/katsu/svn/nowPlayingCd/trunk@2 140ec129-5d39-4364-b83f-a756987d7ee6

---
+++ user_getRecentTracks.php
@@ -0,0 +1,36 @@
+<?php
+
+// Include the API
+require '../lastfmapi/lastfmapi.php';
+
+$authVars['apiKey'] = '';
+
+$config = array(
+	'enabled' => true,
+	'path' => '../lastfmapi/',
+	'cache_length' => 1800
+);
+
+// Pass the array to the auth class to eturn a valid auth
+$auth = new lastfmApiAuth('setsession', $authVars);
+
+$apiClass = new lastfmApi();
+$userClass = $apiClass->getPackage($auth, 'user', $config);
+
+// Setup the variables
+$methodVars = array(
+	'user' => 'oggy-k',
+	'limit' => 1
+);
+
+if ( $tracks = $userClass->getRecentTracks($methodVars) ) {
+	echo '<b>Data Returned</b>';
+	echo '<pre>';
+	print_r($tracks);
+	echo '</pre>';
+}
+else {
+	die('<b>Error '.$userClass->error['code'].' - </b><i>'.$userClass->error['desc'].'</i>');
+}
+
+?>
